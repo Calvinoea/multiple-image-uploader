8d135e95be107fa96af345373f7a6c4d1e989945
calvinoea <calvinebunamu@gmail.com>
2020-07-19 22:19:44 +0100

    Tests carried out wit PHP tinkerer

--- database/migrations/2020_07_18_101008_create_post_images_table.php
+++ database/migrations/2020_07_18_101008_create_post_images_table.php
@@ -1,13 +1,13 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 class CreatePostImagesTable extends Migration
 {
     /**
-     * Run the migrations.
+     * Run the migrations
      *
      * @return void
      */
